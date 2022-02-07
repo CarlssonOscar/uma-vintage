d53ffca8900e5b4b2d737d5222642209a8a1233a
CarlssonOscar <oosccar@gmail.com>
2022-02-07 11:22:01 +0000

    Added filter to categories

--- products/admin.py
+++ products/admin.py
@@ -13,12 +13,12 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
-
 
 
 admin.site.register(Product, ProductAdmin)
