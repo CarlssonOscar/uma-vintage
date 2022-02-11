a3d387d6e35e4076834e7747dd62f0ac1fc87693
CarlssonOscar <oosccar@gmail.com>
2022-02-11 11:47:51 +0000

    Add cart preview, fix and styled toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -12,7 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add products to the cart """
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -29,7 +29,10 @@ def add_to_cart(request, item_id):
 
 def delete_from_cart(request, item_id):
     """Delete the item from the shopping bag"""
+    product = get_object_or_404(Product, pk=item_id)
     cart = request.session.get('cart', {})   
     cart.pop(item_id)
+    messages.success(request, f'Removed {product.name} from your bag')
+    
     request.session['cart'] = cart
     return HttpResponse(status=200)
