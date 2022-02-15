5ee33722bc599c8a3e6dbfcf7642f3e3ba9ced9c
CarlssonOscar <oosccar@gmail.com>
2022-02-15 10:10:16 +0000

    Add stripe elements, add checkout css and style checkout

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KJbMGIwYBgg8NCgFTNu5iP3vU1PSbXabTY2eKz78hSnM7moULIEyqUCCT2xRBI6lwHLD86ob2WdhuvvguIrKK3Z00SRxS5258',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
