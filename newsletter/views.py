0c50f5c86c2dd50757476a148045c44159ddc70a
CarlssonOscar <oosccar@gmail.com>
2022-03-31 11:15:36 +0000

    Create newsletter model, creat newsletter functionality, ongoing

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Newsletter
+from django.http import HttpResponseRedirect
+from .forms import NewsletterForm
 
 # Create your views here.
+def newsletters(request):
+    
+    form_data = {
+            'email': request.POST['email'],
+    }
+    newsletter_form = NewsletterForm(form_data)
+    if newsletter_form.is_valid():
+
+        if request.method == "POST":
+            form = NewsletterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.info(request, "Thanks for signing up to our Newsletter!")
+
+    context = {
+        'email': 'email',
+    }
+    return render(request, template, context)
+    return HttpResponseRedirect(request.path_info)
--- uma/urls.py
+++ uma/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'uma.views.handler404'
