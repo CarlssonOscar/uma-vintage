bc359c1a508cbb81c79139ee2fc77dd09122124e
CarlssonOscar <oosccar@gmail.com>
2022-04-04 09:01:05 +0000

    Remove label and add placeholder to newsletter form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -3,6 +3,8 @@ from .models import Newsletter
 
 
 class NewsletterForm(forms.ModelForm):
+    email = forms.CharField(label='', 
+                    widget=forms.TextInput(attrs={'placeholder': 'Newsletter sign up!'}))
     class Meta:
         model = Newsletter
         fields = ('email',)
